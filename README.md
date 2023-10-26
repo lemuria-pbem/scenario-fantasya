db1b29696952ba45b08623fef8613c344b7af0a2
lemuria-pbem <lemuria@online.de>
2023-10-26 12:37:34 +0200

    [FEATURE] Move scripting from engine package.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lemuria
+
+Lemuria ist ein „Play-by-eMail“, wird also über E-Mail gespielt, und ist
+inspiriert durch Eressea und Fantasya.
+
+Der Programmcode ist in Bibliotheken aufgeteilt. Diese Bibliothek beinhaltet
+eine Erweiterung der Fantasya-Engine um Rollenspielelemente wie die Steuerung
+von Nichtspielercharakteren (NPC) und Aufträge (Quests).
---
+++ src/Storage/ScenarioGame.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+namespace Lemuria\Scenario\Fantasya\Storage;
+
+use Lemuria\Engine\Fantasya\Storage\LemuriaGame;
+use Lemuria\Storage\Ini\SectionList;
+use Lemuria\Storage\IniProvider;
+
+class ScenarioGame extends LemuriaGame
+{
+	private const SCRIPTS_DIR = 'scripts';
+
+	/**
+	 * Get NPC scripts data.
+	 *
+	 * @return array<string, SectionList>
+	 */
+	public function getScripts(): array {
+		$data       = [];
+		$scriptsDir = self::GAME_DIR . DIRECTORY_SEPARATOR . self::SCRIPTS_DIR;
+		$pathPos    = strlen($scriptsDir) + 1;
+		$provider   = new IniProvider($scriptsDir);
+		foreach ($provider->glob() as $path) {
+			$file        = substr($path, $pathPos);
+			$data[$file] = $provider->read($file);
+		}
+		return $data;
+	}
+
+	/**
+	 * Set NPC scripts data.
+	 *
+	 * @var array<string, SectionList> $scripts
+	 */
+	public function setScripts(array $scripts): static {
+		$scriptsDir = self::GAME_DIR . DIRECTORY_SEPARATOR . self::SCRIPTS_DIR;
+		$provider   = new IniProvider($scriptsDir);
+		foreach ($scripts as $file => $data) {
+			$provider->write($file, $data);
+		}
+		return $this;
+	}
+}
